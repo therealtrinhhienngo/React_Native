f6e95cffa8278a52804cb8f7da9f7b2f36375422
therealtrinhhienngo <trinhhienngo306@gmail.com>
2023-02-06 20:25:02 +0700

    lab2

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [isStateOpen, setStateOpen] = useState(true);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+          source={
+              {uri: 'https://picsum.photos/200/300'}
+          }
+      />
+
+      <View>
+        <Button
+          title='Quản Lí'
+        />
+
+        <Button
+          title='About'
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
